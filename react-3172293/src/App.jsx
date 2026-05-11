1082ea62fe689b88445fe780daf97289224b9075
victorinho23 <vcortezcastrillon7a@gmail.com>
2026-05-11 09:04:12 -0500

    chore(base):codigo base de nuestro proyecto

---
+++ react-3172293/src/App.jsx
@@ -0,0 +1,9 @@
+export default function app(){
+  return(
+    <div className="min-h-screen bg-green-800 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+          Con Victor programar se siente rico y talwind funciona full
+      </h1>
+    </div>
+  );
+}
