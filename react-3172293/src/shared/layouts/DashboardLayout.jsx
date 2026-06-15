e22147e0d9821b3665d06027095cd4633447dde8
victorinho23 <vcortezcastrillon7a@gmail.com>
2026-06-15 15:34:33 -0500

    feat(create-user):Se implemento la creacion de usuarios en el dashboard

---
+++ react-3172293/src/shared/layouts/DashboardLayout.jsx
@@ -0,0 +1,24 @@
+import {Outlet} from "react-router-dom";
+import authBg from "@/assets/images/cafe.png";
+import { UserRegisterForm } from "@/features/users";
+
+export default function DashboardLayout(){
+    return(
+        <>
+            <div
+                className="min-h-screen w-full"
+                style ={{
+                    backgroundImage: `url(${authBg})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+            }}
+        >
+                <main className="mx-auto">
+                    
+                    <UserRegisterForm className=""/>
+                 <Outlet />
+                </main>
+            </div>
+        </>
+    );
+}
