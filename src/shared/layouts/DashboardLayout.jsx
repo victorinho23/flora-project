ff36500eeb813140ae5aaca930e0c1d705602bdb
victorinho23 <vcortezcastrillon7a@gmail.com>
2026-07-12 12:22:07 -0500

    se realizaron cambios

--- src/shared/layouts/DashboardLayout.jsx
+++ src/shared/layouts/DashboardLayout.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import {Outlet} from "react-router-dom";
 import authBg from "@/assets/images/cafe.png";
 import { UserRegisterForm } from "@/features/users";
-import {Button} from "@/shared";
-import { DataTable } from "@/shared"
-import { users } from "../../features/users/data/users";
-import { UserColumns } from "../../features/users/table/UsersColumns";  
-import {ReportConfigModal} from "@/features/users";
+
 
 export default function DashboardLayout(){
-    const [report, setReport] = useState(false)
+    
 
     return(
         <>
@@ -24,23 +19,6 @@ export default function DashboardLayout(){
                 <main className="mx-auto">
                     
                     <UserRegisterForm className=""/>
-                                  <ReportConfigModal
-                    isOpen={report}
-                    onClose={() => setReport(false)}
-                />
-                
-                   <Button 
-                    variant="primary"
-                    size="md"
-                    type="button"
-                    onClick={() => {
-                      console.log("Se oprimio el boton");
-                      setReport(true)
-                    }}
-                    > 
-                    Crear reporte
-                </Button>
-                <DataTable data={users} columns={UserColumns}/>
                  <Outlet />
                 </main>
             </div>
